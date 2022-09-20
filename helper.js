4f2cb995af8774b87abb775586ebd4a3f825abb1
XHunter <kartikey@koders.in>
2022-09-20 22:35:40 +0530

    Update helper.js

--- helper.js
+++ helper.js
@@ -67,7 +67,7 @@ const getBudget = async (apiKey, issueIdentifier) => {
     for (let i = 0; i < tableItems.length; i++) {
       const el = tableItems[i];
       if ($(el).children("th").text() === "Budget") {
-        return $(el).children("td").text().trim();
+        return $(el).children("td").text().trim().replace(/[,.₹]/g, '');
       }
     }
     return null;
