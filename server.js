43eee766d8dac53a01fc01603add5fa00b330f0f
Kartikey Kushwah <kartikey@koders.in>
2022-09-21 12:52:40 +0530

    Added universal CORS for testing

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const { fetchProject, getBudget} = require("./helper");
 const appUrl = process.env.APP_URL;
 const port = 9442;
 
-app.use(cors({
-    origin: appUrl
-}));
+// app.use(cors({
+//     origin: appUrl
+// }));
+
+app.use(cors()); // ! Remove in production
 
 app.use(express.json());
 
