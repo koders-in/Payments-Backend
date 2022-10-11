2de202565218b3a4182d38a8e47e85d1c74a859b
Kartikey Kushwah <kartikey@koders.in>
2022-10-11 10:25:27 +0530

    - Added ESLinter with Coding Styles - Refactored code according to the eslinter

--- server.js
+++ server.js
@@ -1,83 +1,83 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const stripe = require("stripe")(process.env.STRIPE_SK);
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const stripe = require('stripe')(process.env.STRIPE_SK)
 
-const { fetchProject, getProjectData, getBudget } = require("./helper");
-const appUrl = process.env.APP_URL;
-const port = 9442;
+const { fetchProject, getProjectData, getBudget } = require('./helper')
+const appUrl = process.env.APP_URL
+const port = 9442
 
 app.use(
   cors({
-    origin: appUrl,
+    origin: appUrl
   })
-);
+)
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (_, res) => {
-  res.send("Payment API is working perfectly");
-});
+app.get('/', (_, res) => {
+  res.send('Payment API is working perfectly')
+})
 
-app.post("/get-project", async (req, res) => {
-  const { apiKey, projectIdentifier } = req.body;
+app.post('/get-project', async (req, res) => {
+  const { apiKey, projectIdentifier } = req.body
   if (apiKey && projectIdentifier) {
-    const data = await fetchProject(apiKey, projectIdentifier);
-    if (data !== null && data !== "")
-      res.status(200).json({ msg: "Project Details", data: data });
-    else res.status(400).json({ msg: "Bad request" });
-  } else res.status(404).json({ msg: "Some keys are missing", data: null });
-});
+    const data = await fetchProject(apiKey, projectIdentifier)
+    if (data !== null && data !== '') {
+      res.status(200).json({ msg: 'Project Details', data })
+    } else res.status(400).json({ msg: 'Bad request' })
+  } else res.status(404).json({ msg: 'Some keys are missing', data: null })
+})
 
-app.post("/get-budget", async (req, res) => {
-  const { apiKey, issues } = req.body;
+app.post('/get-budget', async (req, res) => {
+  const { apiKey, issues } = req.body
   if (apiKey && issues) {
-    const amount = await getBudget(apiKey, issues);
-    if (amount === null && amount === "")
-      res.status(400).json({ msg: "Bad request" });
-    else res.status(200).json({ msg: "Budget amount", data: amount });
-  } else res.status(404).json({ msg: "Some keys are missing", data: null });
-});
+    const amount = await getBudget(apiKey, issues)
+    if (amount === null && amount === '') {
+      res.status(400).json({ msg: 'Bad request' })
+    } else res.status(200).json({ msg: 'Budget amount', data: amount })
+  } else res.status(404).json({ msg: 'Some keys are missing', data: null })
+})
 
-app.post("/checkout", async (req, res) => {
+app.post('/checkout', async (req, res) => {
   const { milestoneTitle, milestoneUnitAmount, apiKey, projectIdentifier } =
-    req.body;
+    req.body
   const { projectName, projectIcon } = await getProjectData(
     apiKey,
     projectIdentifier
-  );
+  )
 
   if ((milestoneTitle && milestoneUnitAmount) || projectIcon) {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: "inr",
+            currency: 'inr',
             product_data: {
               name: projectName,
               description: milestoneTitle,
-              images: [projectIcon],
+              images: [projectIcon]
             },
-            unit_amount: milestoneUnitAmount * 100,
+            unit_amount: milestoneUnitAmount * 100
           },
-          quantity: 1,
-        },
+          quantity: 1
+        }
       ],
-      mode: "payment",
+      mode: 'payment',
       success_url: `${appUrl}/#/success`,
-      cancel_url: `${appUrl}/`,
-    });
-    res.status(200).json({ msg: "Checkout URL", data: session.url });
-  } else res.status(404).json({ msg: "Some keys are missing", data: null });
-});
+      cancel_url: `${appUrl}/`
+    })
+    res.status(200).json({ msg: 'Checkout URL', data: session.url })
+  } else res.status(404).json({ msg: 'Some keys are missing', data: null })
+})
 
-app.get("*", function (req, res) {
-  const msg = `<p>No possible <b>${req.path} </b> endpoint for <b>${req.method}</b> method</p>`;
-  res.status(404).send(msg);
-});
+app.get('*', function (req, res) {
+  const msg = `<p>No possible <b>${req.path} </b> endpoint for <b>${req.method}</b> method</p>`
+  res.status(404).send(msg)
+})
 
 app.listen(port, () => {
-  console.log(`Koders payment app listening at http://localhost:${port}`);
-});
+  console.log(`Koders payment app listening at http://localhost:${port}`)
+})
