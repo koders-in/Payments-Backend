d563505d3b004d92b31bc6a53248546b634ae78f
saksham <sakshamchauhan79@gmail.com>
2023-04-06 19:18:59 +0530

    feat:added mail template for subscription and contact query for user

--- mail.js
+++ mail.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
 
 async function sendEmail(contactObj) {
   try {
@@ -20,6 +22,33 @@ async function sendEmail(contactObj) {
         subject: "User Inquiry through Contact Us",
         text: `Dear Raghav,\n\nI hope this email finds you well. I am reaching out to inform you that we have received an inquiry through the Contact Us section of your website. The details of the inquiry are as follows:\n\nName:${contactObj.name}\nContact Details:${contactObj.phone}\n\nMessage:${contactObj.message}\n\nWe appreciate your efforts in creating a user-friendly platform for customers to connect with your brand. We will be taking prompt action to respond to the user and address their concerns accordingly.\n\nThank you for your attention to this matter.\n\nBest regards,\n${contactObj.name}\n${contactObj.phone}\n${contactObj.email}`,
       });
+      const source = fs.readFileSync("./mail_assets/contact.hbs", "utf8");
+      const template = handlebars.compile(source);
+      const html = template({ user: contactObj.name });
+      if (contactObj.email)
+        await transporter.sendMail({
+          from: process.env.EMAIL_USERNAME,
+          to: contactObj.email,
+          subject: "User Inquiry through Contact Us",
+          html,
+          attachments: [
+            {
+              filename: "white-logo.png",
+              path: "./mail_assets/white-logo.png",
+              cid: "white-logo.png",
+            },
+            {
+              filename: "instagram.png",
+              path: "./mail_assets/instagram.png",
+              cid: "instagram.png",
+            },
+            {
+              filename: "facebook.png",
+              path: "./mail_assets/facebook.png",
+              cid: "facebook.png",
+            },
+          ],
+        });
     } else {
       res = await transporter.sendMail({
         from: process.env.EMAIL_USERNAME,
@@ -27,6 +56,33 @@ async function sendEmail(contactObj) {
         subject: "Request for Subscription",
         text: `Dear Raghav,\n\nI want to subscribing your newsletter! \n\n Email address:${contactObj?.email}`,
       });
+      const source = fs.readFileSync("./mail_assets/subscribe.hbs", "utf8");
+      const template = handlebars.compile(source);
+      const html = template({ user: contactObj.name });
+      if (contactObj.email)
+        await transporter.sendMail({
+          from: process.env.EMAIL_USERNAME,
+          to: contactObj.email,
+          subject: "User Inquiry through Contact Us",
+          html,
+          attachments: [
+            {
+              filename: "white-logo.png",
+              path: "./mail_assets/white-logo.png",
+              cid: "white-logo.png",
+            },
+            {
+              filename: "instagram.png",
+              path: "./mail_assets/instagram.png",
+              cid: "instagram.png",
+            },
+            {
+              filename: "facebook.png",
+              path: "./mail_assets/facebook.png",
+              cid: "facebook.png",
+            },
+          ],
+        });
     }
     if (res?.messageId || res?.response?.includes("OK")) {
       return { data: res };
