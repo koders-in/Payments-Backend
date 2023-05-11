2959bfba37f57601407e9851b0a7be9ee4b08da2
XHunter <kushwah.kartikey14@upesalumni.upes.ac.in>
2023-05-11 12:20:52 +0530

    refactor(eslint): refactored helper.js

--- helper.js
+++ helper.js
@@ -1,176 +1,174 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const client = require("./axios");
-const cheerio = require("cheerio");
-const { makeConfig, getValueFromArray, fetchData } = require("./utils");
+const client = require('./axios')
+const cheerio = require('cheerio')
+const { makeConfig, getValueFromArray, fetchData } = require('./utils')
 
 const getMilestoneData = async (apiKey, milestone) => {
-  const milestoneData = {};
+  const milestoneData = {}
   try {
     const { data, status } = await client.get(
       `/versions/${milestone}.json`,
       makeConfig(apiKey)
-    );
+    )
     if (status === 200) {
-      const { version } = data;
+      const { version } = data
       milestoneData[version.id] = {
         title: version.name,
         description: version.description,
         status: version.status,
         dueDate: version.due_date,
-        demoLink: getValueFromArray(version.custom_fields, "Demo Link"),
-        filesLink: getValueFromArray(version.custom_fields, "Files Link"),
+        demoLink: getValueFromArray(version.custom_fields, 'Demo Link'),
+        filesLink: getValueFromArray(version.custom_fields, 'Files Link'),
         paymentStatus: getValueFromArray(
           version.custom_fields,
-          "Payment Status"
-        ),
-      };
-      return milestoneData;
+          'Payment Status'
+        )
+      }
+      return milestoneData
     }
   } catch (err) {
-    console.log("Milestone not found. Skipping...");
-    return null;
+    console.log('Milestone not found. Skipping...')
+    return null
   }
-  return null;
-};
+  return null
+}
 
 const getProjectMilestones = async (apiKey, projectIdentifier) => {
-  const milestonesData = {};
+  const milestonesData = {}
   try {
     const response = await client.get(
       `/projects/${projectIdentifier}/issues.json?status_id=*&limit=100`,
       makeConfig(apiKey)
-    );
-    for (let issue of response.data.issues) {
+    )
+    for (const issue of response.data.issues) {
       try {
-        if (milestonesData[issue.fixed_version.id] === undefined)
+        if (milestonesData[issue.fixed_version.id] === undefined) {
           milestonesData[issue.fixed_version.id] = {
             doneRatio: issue.done_ratio,
-            issues: [issue.id],
-          };
-        else
+            issues: [issue.id]
+          }
+        } else {
           milestonesData[issue.fixed_version.id] = {
             doneRatio:
               milestonesData[issue.fixed_version.id].doneRatio +
               issue.done_ratio,
             issues: [
               ...milestonesData[issue.fixed_version.id].issues,
-              issue.id,
-            ],
-          };
+              issue.id
+            ]
+          }
+        }
       } catch (error) {
-        console.log("Something went wrong. Unable to find any releases", error);
-        return null;
+        console.log('Something went wrong. Unable to find any releases', error)
+        return null
       }
     }
-    return milestonesData;
+    return milestonesData
   } catch (error) {
-    console.error("Something went wrong while fetching milestone data.", error);
-    return null;
-  }
-};
-
-const getAllProjectStatus = async (apiKey) => {
-  const blackListedProjects = ["public-relations", "kore", "x12-mirror", "graphana", "test-project-budget-check", "wait-list", "getting-started-with-koders"];
-  try{
-    const {data, status} = await client.get('/projects.json', makeConfig(apiKey));
-    if(status === 200){
-      const {projects} = data;
-      const projectStatus = {};
-      for(let project of projects){
-        if(blackListedProjects.includes(project.identifier)) {
-          continue;
-        }
-        projectStatus[project.name] = 0
-        const response = await client.get(`/projects/${project.identifier}/issues.json?status_id=*&limit=100`, makeConfig(apiKey));
-        let issueCounter = 0;
-        if (response) {
-        for(let issue of response.data.issues){
-          projectStatus[project.name] += issue.done_ratio;
-          issueCounter += 1;
-        }
-      }
-        if( issueCounter > 0) {
-        projectStatus[project.name] /= issueCounter;
-        projectStatus[project.name] = projectStatus[project.name].toFixed(2);
-        issueCounter = 0;
-      }
-    }
-      return projectStatus;
-      }
-  }catch(error){
-    console.error("Something went wrong while fetching project status.", error);
-    return null;
+    console.error('Something went wrong while fetching milestone data.', error)
+    return null
   }
 }
 
-
+const getAllProjectStatus = async (apiKey) => {
+  const blackListedProjects = ['public-relations', 'kore', 'x12-mirror', 'graphana', 'test-project-budget-check', 'wait-list', 'getting-started-with-koders']
+  try {
+    const { data, status } = await client.get('/projects.json', makeConfig(apiKey))
+    if (status === 200) {
+      const { projects } = data
+      const projectStatus = {}
+      for (const project of projects) {
+        if (blackListedProjects.includes(project.identifier)) {
+          continue
+        }
+        projectStatus[project.name] = 0
+        const response = await client.get(`/projects/${project.identifier}/issues.json?status_id=*&limit=100`, makeConfig(apiKey))
+        let issueCounter = 0
+        if (response) {
+          for (const issue of response.data.issues) {
+            projectStatus[project.name] += issue.done_ratio
+            issueCounter += 1
+          }
+        }
+        if (issueCounter > 0) {
+          projectStatus[project.name] /= issueCounter
+          projectStatus[project.name] = projectStatus[project.name].toFixed(2)
+        }
+      }
+      return projectStatus
+    }
+  } catch (error) {
+    console.error('Something went wrong while fetching project status.', error)
+    return null
+  }
+}
 
 const getProjectData = async (apiKey, projectIdentifier) => {
   try {
     const { data, status } = await client.get(
       `/projects/${projectIdentifier}.json`,
       makeConfig(apiKey)
-    );
+    )
 
-    let projectData = {};
+    const projectData = {}
     if (status === 200) {
-      const { project } = data;
-      projectData["projectName"] = project.name;
-      projectData["description"] = project.description;
-      projectData["projectIcon"] = getValueFromArray(
+      const { project } = data
+      projectData.projectName = project.name
+      projectData.description = project.description
+      projectData.projectIcon = getValueFromArray(
         project.custom_fields,
-        "Project Icon"
-      );
-      return projectData;
+        'Project Icon'
+      )
+      return projectData
     }
   } catch (error) {
-    console.error(error);
-    return null;
+    console.error(error)
+    return null
   }
-};
+}
 
 const getBudget = async (apiKey, issueIdentifiers) => {
-  let amount = 0;
-  for (let issue of issueIdentifiers) {
+  let amount = 0
+  for (const issue of issueIdentifiers) {
     try {
       const { data } = await client.get(
         `/issues/${issue}?token=${apiKey}`,
         makeConfig(apiKey, true)
-      );
+      )
 
-      const $ = cheerio.load(data);
-      const tableItems = $(".billing-details tbody tr");
+      const $ = cheerio.load(data)
+      const tableItems = $('.billing-details tbody tr')
       for (let i = 0; i < tableItems.length; i++) {
-        const el = tableItems[i];
-        if ($(el).children("th").text() === "Budget") {
+        const el = tableItems[i]
+        if ($(el).children('th').text() === 'Budget') {
           amount += Number(
-            $(el).children("td").text().trim().replace(/[,₹]/g, "")
-          );
+            $(el).children('td').text().trim().replace(/[,₹]/g, '')
+          )
         }
       }
     } catch (error) {
-      console.log("Something went wrong while calculating budget. Skipping...");
-      return null;
+      console.log('Something went wrong while calculating budget. Skipping...')
+      return null
     }
   }
-  return amount;
-};
+  return amount
+}
 
 const fetchProject = async (apiKey, projectIdentifier) => {
   try {
-    const project = {};
-    const projectData = await getProjectData(apiKey, projectIdentifier);
+    const project = {}
+    const projectData = await getProjectData(apiKey, projectIdentifier)
     const projectMilestones = await getProjectMilestones(
       apiKey,
       projectIdentifier
-    );
+    )
 
-    project[projectIdentifier] = { projectData: projectData };
+    project[projectIdentifier] = { projectData }
 
-    const milestones = {};
-    for (let milestone in projectMilestones) {
-      const milestoneData = await getMilestoneData(apiKey, milestone);
+    const milestones = {}
+    for (const milestone in projectMilestones) {
+      const milestoneData = await getMilestoneData(apiKey, milestone)
       milestones[milestone] = {
         title: milestoneData[milestone].title,
         description: milestoneData[milestone].description,
@@ -180,67 +178,66 @@ const fetchProject = async (apiKey, projectIdentifier) => {
         doneRatio: projectMilestones[milestone].doneRatio,
         paymentStatus: milestoneData[milestone].paymentStatus,
         demoLink: milestoneData[milestone].demoLink,
-        filesLink: milestoneData[milestone].filesLink,
-      };
+        filesLink: milestoneData[milestone].filesLink
+      }
     }
-    project[projectIdentifier].milestones = milestones;
-    return project;
+    project[projectIdentifier].milestones = milestones
+    return project
   } catch (error) {
     console.log(
-      "Something went wrong while fetching project details. Skipping...",
+      'Something went wrong while fetching project details. Skipping...',
       error
-    );
-    return null;
+    )
+    return null
   }
-};
-
+}
 
 const getTagsFromIssues = async (apiKey, issues, targtedTag) => {
-  if (issues === undefined) return false;
+  if (issues === undefined) return false
   for (const issue of issues) {
     const { data, status } = await client.get(
       `/issues/${issue}.json`,
       makeConfig(apiKey)
-    );
+    )
     if (status === 200) {
-      const tags = data["issue"]["tags"];
+      const tags = data.issue.tags
       for (const tag of tags) {
-        if (tag.name.toLowerCase().includes(targtedTag.toLowerCase()))
-          return true;
+        if (tag.name.toLowerCase().includes(targtedTag.toLowerCase())) {
+          return true
+        }
       }
     }
   }
+  return false
+}
 
-  return false;
-};
-
-async function getInvoiceDetails(project, apiKey) {
+async function getInvoiceDetails (project, apiKey) {
   try {
-    if (!project) return null;
-    const projectDetails = await fetchData(`/projects/${project}.json`, apiKey);
+    if (!project) return null
+    const projectDetails = await fetchData(`/projects/${project}.json`, apiKey)
     if (projectDetails !== null) {
       const invoiceField = projectDetails?.project?.custom_fields.filter(
-        (item) => item.name === "Invoice id"
-      );
+        (item) => item.name === 'Invoice id'
+      )
       const invoiceDetails = await fetchData(
         `/invoices/${invoiceField[0].value}.json`,
         apiKey
-      );
+      )
       if (invoiceDetails !== null) {
         const contactDetails = await fetchData(
           `/contacts/${invoiceDetails?.invoice?.contact?.id}.json`,
           apiKey
-        );
+        )
         return {
           projectData: projectDetails?.project,
           invoiceData: invoiceDetails.invoice,
-          contactDetails: contactDetails ? contactDetails?.contact : null,
-        };
-      } else return null;
-    } else return null;
+          contactDetails: contactDetails ? contactDetails?.contact : null
+        }
+      } else return null
+    } else return null
   } catch (error) {
-    console.log(error);
-    return null;
+    console.log(error)
+    return null
   }
 }
 
@@ -250,5 +247,5 @@ module.exports = {
   getProjectData,
   getTagsFromIssues,
   getInvoiceDetails,
-  getAllProjectStatus,
-};
+  getAllProjectStatus
+}
