d802b8610f74573615d7544e60632d8eb226ba3b
saksham <sakshamchauhan79@gmail.com>
2023-06-23 14:32:57 +0530

    minor changes in options

--- src/invoice/helper.js
+++ src/invoice/helper.js
@@ -15,7 +15,7 @@ function generatePDF(response) {
       });
       const options = {
         format: "Letter",
-        phantomPath: "./node_modules/phantomjs-prebuilt/bin/phantomjs",
+        phantomPath: "/usr/local/bin/phantomjs",
       };
       const uniqueName = uuidv4();
       const path = `./pdf/${uniqueName}.pdf`;
